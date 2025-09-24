fb64b00e56ccf435c41f94da086da07aa46d2506
Greenskulllord <gavinfaddis1@gmail.com>
2025-09-24 13:34:36 -0700

    added math for the roller

--- src/Dice_Roller.java
+++ src/Dice_Roller.java
@@ -1,18 +1,25 @@
+/*
+    To Do:
+
+    -   Methods to calculate highest, lowest, average, and median.
+    They should take an integer array and should return integers for highest and lowest and floats for average and median.
+
+    -   A method that takes a string prompt and returns an integer that the user inputs.
+ */
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
 
-public class Main {
+public class Dice_Roller {
     public static void main(String[] args) {
         //declare basic variable
         Random Rand = new Random();
 
         //for the end of program when user wants to reset the program
-        boolean Reset_Input = true;
+        boolean Reset_Input;
 
         //Set scanner(user input function) as sc and get it ready
         Scanner sc = new Scanner(System.in);
-
 
         //loop it in a do-while so a reset is possible
         do {
@@ -22,7 +29,7 @@ public class Main {
             //declare variables but there special cause there in a loop
             int Rolls = 0;
             int Sides = 0;
-            Boolean ValidInput = false;
+            boolean ValidInput = false;
 
             //start the actual thingy, thing
             System.out.println("-----------------------Welcome To The Dice Roller-----------------------");
@@ -67,14 +74,52 @@ public class Main {
 
                 //randomize the input so the max is user's choice
                 int new_side = Rand.nextInt(Sides) + 1;
+
                 //store each random number into an array
                 Results[_i] = new_side;
-                System.out.println("die " + (_i + 1) + ": " + new_side);
+
+                //not really needed for assignment but just produces results in a top to down format instead of a array but both is added
+                System.out.println("die " + (_i + 1) + ": " + Results[_i]);
 
             }
 
+
+
+            //find highest, lowest, and average number from results
+            int lowestNumber = Sides, highestNumber = 0;
+            float sum = 0;
+
+            for (int i : Results) {
+                if (i > highestNumber) {
+                    highestNumber = i;
+                }
+
+                if (i < lowestNumber) {
+                    lowestNumber = i;
+                }
+                sum += i;
+            }
+
+            float average = sum / Rolls;
+
             System.out.println("-----------------------------------------------------------------------------------");
             System.out.println("Results: " + Arrays.toString(Results));
+            System.out.println("max = " + highestNumber + "\nmin = " + lowestNumber + "\naverage = " + average);
+
+
+            //sort array
+            Arrays.sort(Results);
+
+            int median;
+            int middleIndex = Results.length / 2;
+
+            if (Results.length % 2 == 0) {
+                median = (middleIndex - 1);
+            } else {
+                median = middleIndex;
+            }
+
+            System.out.println("median = " + median);
             System.out.println("-----------------------------------------------------------------------------------");
 
             //end code or restart the code(in progress)
@@ -98,8 +143,11 @@ public class Main {
 
                 }
             } catch (Exception e) {
+
+                //error catcher isnt working but program works fine even if user puts "a" or "b". it counts as 'no'
                 System.out.println("Error - Please put 'yes' or 'no'");
                 sc.nextLine();
+
             }
         }
         //THIS FUCKING WHILE LOOP WONT WORK FUCKKK. WORK!! WHY ARENT YOU WORKING
